156288fbc247ec82fd0553b8415782b85dd89eb5
Saajith22 <mohammedsaajith93@gmail.com>
2022-07-07 11:29:27 +0400

    simple 404 :)

---
+++ code/index.js
@@ -0,0 +1,7 @@
+const heading = document.querySelector("h1");
+const img = document.querySelector("img");
+
+window.onload = () => {
+  img.style.transform = "rotate(360deg)";
+  heading.style.opacity = "100%";
+};
